d1ec99a47271a7d880939e9f164667a9bd2ce414
sommano@gmail.com <sommano@gmail.com>
2020-05-08 17:39:26 -0500

    Components Part 1

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>ABAAA Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <Main />
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0fA',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
