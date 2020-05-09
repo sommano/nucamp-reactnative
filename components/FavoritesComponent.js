92f60093da0ede6a32029c67ed92b893df7c2b32
sommano@gmail.com <sommano@gmail.com>
2020-05-09 11:22:08 -0500

    Swipeout and Delete Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,12 +4,19 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
         favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {            //anytime we want to dispatch an actioncreator from a component we need to mapdispatchtoprop object
+    deleteFavorite: campsiteId => (deleteFavorite(campsiteId))
+
 };
 
 class Favorites extends Component {
@@ -19,28 +26,41 @@ class Favorites extends Component {
     }
 
     render() {
+
         const { navigate } = this.props.navigation;
-        const renderFavoriteItem = ({item}) => {
+        const renderFavoriteItem = ({ item }) => {
+
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ]
             return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
-                />
-            );
-        };
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    />
+                </Swipeout>
+            )
+        }
 
         if (this.props.campsites.isLoading) {
             return <Loading />;
         }
+
         if (this.props.campsites.errMess) {
             return (
                 <View>
                     <Text>{this.props.campsites.errMess}</Text>
                 </View>
-            );
+            )
         }
+
         return (
             <FlatList
                 data={this.props.campsites.campsites.filter(
@@ -49,8 +69,9 @@ class Favorites extends Component {
                 renderItem={renderFavoriteItem}
                 keyExtractor={item => item.id.toString()}
             />
-        );
+        )
     }
+
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
